32057fc615d4ad283689d922e9d322e04b44b745
Amal Koraishy <amalkoraishy@icloud.com>
2024-07-26 11:51:16 -0400

    Initial Commit

---
+++ Blah/BlahApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlahApp.swift
+//  Blah
+//
+//  Created by Amal Koraishy on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct BlahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
